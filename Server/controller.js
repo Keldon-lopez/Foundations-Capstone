5e447ec73dca0933e780ea2f0cc88a9debf495db
Keldon Lopez <keldon.lopz@gmail.com>
2023-04-18 19:56:40 -0600

    Created seed function and ran seed function. Successfully seeded database

--- Server/controller.js
+++ Server/controller.js
@@ -14,24 +14,26 @@ const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
 module.exports = {
     seed: (req, res) => {
         sequelize.query(`
-            drop table if exists cities;
-            drop table if exists countries;
+            drop table if exists users;
+            drop table if exists tickets;
+
             create table users (
                 user_id serial primary key, 
-                username VARCHAR(40) NOT NULL
+                username varchar NOT NULL UNIQUE
             );
             create table tickets (
                 ticket_id serial primary key,
-                username string REFERENCES users(username),
-                priority integer NOT NULL,
-                issue_type string NOT NULL,
-                status string NOT NULL,
-                description string NOT NULL                
+                username varchar REFERENCES users(username),
+                priority integer ,
+                type varchar ,
+                status varchar ,
+                description varchar                 
             );
+
             insert into users (username)
             values ('testUser');
 
-            insert into tickets (username, priority, issue_type, status, description) 
+            insert into tickets (username, priority, type, status, description) 
             values ('testUser', 1, 'computer', 'open', 'This is a test ticket');
         `).then(() => {
             console.log('DB seeded!')
--- Server/server.js
+++ Server/server.js
@@ -5,8 +5,14 @@ const cors = require('cors')
 const {SERVER_PORT} = process.env
 const {seed, getTickets} = require('./controller.js')
 
+app.use(express.json())
+app.use(cors())
+
 // DEV
 app.post('/seed', seed)
 
 // COUNTRIES
 app.get('/tickets', getTickets)
+
+
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
